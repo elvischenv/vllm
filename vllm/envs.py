6c2c56edb1df49227383b97c5ceccff882a0c407
elvischenv <219235043+elvischenv@users.noreply.github.com>
2026-03-14 07:30:11 -0700

    add VLLM_USE_FLASHINFER_ROPE
    Signed-off-by: elvischenv <219235043+elvischenv@users.noreply.github.com>

--- vllm/envs.py
+++ vllm/envs.py
@@ -256,6 +256,7 @@ if TYPE_CHECKING:
     VLLM_MEMORY_PROFILER_ESTIMATE_CUDAGRAPHS: bool = True
     VLLM_NIXL_EP_MAX_NUM_RANKS: int = 32
     VLLM_XPU_ENABLE_XPU_GRAPH: bool = False
+    VLLM_USE_FLASHINFER_ROPE: bool = False
     VLLM_LORA_ENABLE_DUAL_STREAM: bool = False
 
 
@@ -1703,6 +1704,10 @@ environment_variables: dict[str, Callable[[], Any]] = {
     # Enable simple KV offload.
     "VLLM_USE_SIMPLE_KV_OFFLOAD": lambda: bool(
         int(os.getenv("VLLM_USE_SIMPLE_KV_OFFLOAD", "0"))
+    ),
+    # If set to 1, use the FlashInfer's rotary embedding kernel
+    "VLLM_USE_FLASHINFER_ROPE": lambda: bool(
+        int(os.getenv("VLLM_USE_FLASHINFER_ROPE", "0"))
     ),
     # Whether to enable dual cuda streams for LoRA computation
     "VLLM_LORA_ENABLE_DUAL_STREAM": lambda: bool(
